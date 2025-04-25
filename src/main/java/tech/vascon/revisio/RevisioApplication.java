b8086d8b9a4d633bdc5e864bf2d862facb3f7f70
Cabavas <cabavas@gmail.com>
2025-04-25 01:08:43 -0300

    project created

---
+++ src/main/java/tech/vascon/revisio/RevisioApplication.java
@@ -0,0 +1,13 @@
+package tech.vascon.revisio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/vascon/revisio/RevisioApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.vascon.revisio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
